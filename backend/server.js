4435ef289c012ea05abf1080639a179dfbacceb6
AlexeiP <alexei.penaranda@ucb.edu.bo>
2026-09-06 19:29:21 -0400

    [feat] (backend) Backend mejorado: Arreglo de dependencias y limites para subir archivos de imagenes

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 4000;
 
 // ---------- Middlewares globales ----------
 app.use(cors({ origin: process.env.CORS_ORIGIN || "*" }));
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(morgan("dev"));
 
 // ---------- Rutas ----------
